54094b60aed0417db477815ef21e47058e258067
Ubuntu <ubuntu@ip-172-31-26-155.us-west-2.compute.internal>
2020-06-17 07:04:08 +0000

    added cors

--- server/index.js
+++ server/index.js
@@ -5,7 +5,11 @@ const app = express();
 app.use(express.urlencoded());
 app.use(express.json());
 app.use(pino);
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 const getScoreBoard = require('./api/getScoreBoard.js')
 const getBoxScore = require('./api/getBoxScore.js')
